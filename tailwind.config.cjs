07b96457e3301ef9653ec6ceddc2632c1ae08e7c
vitordcode <vitordcode@gmail.com>
2023-02-15 20:00:07 -0300

    projeto configurado

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#0D0D0D',
+      },
+      fontFamily: {
+        poppins: 'Poppins', 'sans-serif'
+      },
+    },
+  },
+  plugins: [],
+}
