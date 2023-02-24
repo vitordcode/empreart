77affcf96441aa3148ae9c3e866b46b2a2107bf2
vitordcode <vitordcode@gmail.com>
2023-02-24 20:50:01 -0300

    criado componente quem somos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import Marks from './components/Marks'
+import QuemSomos from './components/QuemSomos'
 import { Services } from './components/Services'
 
 import './styles/global.css'
@@ -11,6 +12,7 @@ function App() {
     <>
       <Header />
       <Services />
+      <QuemSomos />
       <Marks />
       <Footer />
     </>
