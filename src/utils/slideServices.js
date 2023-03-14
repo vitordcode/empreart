820a0769fa58c4f022fbd8ea6830aa703d40c7dc
vitordcode <vitordcode@gmail.com>
2023-03-13 22:18:19 -0300

    Adicionando novos servicos nos slides

--- src/utils/slideServices.js
+++ src/utils/slideServices.js
@@ -6,54 +6,111 @@ import pingodegente from "../../assets/slide/pingodegente.png";
 import specialle from "../../assets/slide/specialle.png";
 import vetspets from "../../assets/slide/vetspets.png";
 
+import bancabady from "../../assets/slide/BANCABADY.png";
+import erika from "../../assets/slide/ERIKA.png";
+import insenf from "../../assets/slide/INSENF.png";
+import kadock from "../../assets/slide/KADOCK.png";
+import limalar from "../../assets/slide/LimaLar.png";
+import venite from "../../assets/slide/VENITE.png";
+import vetspets2 from "../../assets/slide/VETSPETS2.png";
+
 export const slideServices = [
   {
     id: 1,
     image: forma,
-    title: "Forma Academia",
-    local: "Bady Bassit - SP",
-    content: "Montagem de Arte, Impressão e Instalação de Lona em Estrutura"
+    title: "Academia Forma",
+    local: "Bady Bassitt - SP",
+    content: "Montagem de arte, impressão e instalação de adesivos."
   },
   {
     id: 2,
     image: mariah,
     title: "Mariah",
-    local: "Bady Bassit - SP",
+    local: "Bady Bassitt - SP",
     content: "Montagem de Arte, Impressão e Instalação de Lona em Estrutura"
   },
   {
     id: 3,
     image: merceariabady,
-    title: "Mercearia Bady Bassit",
-    local: "Bady Bassit - SP",
-    content: "Montagem de Arte, Impressão e Instalação de Lona em Estrutura"
+    title: "Mercearia Bady",
+    local: "Bady Bassitt - SP",
+    content: "Montagem de arte e impressão de lona."
   },
   {
     id: 4,
     image: mlife,
-    title: "MLife",
-    local: "Bady Bassit - SP",
-    content: "Montagem de Arte, Impressão e Instalação de Lona em Estrutura"
+    title: "Academia M-Life Fitness",
+    local: "Bady Bassitt - SP",
+    content: "Montagem de arte, impressão e instalação de adesivos."
   },
   {
     id: 5,
     image: pingodegente,
-    title: "Pingo de Gente",
-    local: "Bady Bassit - SP",
-    content: "Montagem de Arte, Impressão e Instalação de Lona em Estrutura"
+    title: "Escola Infantil Pingo de Gente",
+    local: "Bady Bassitt- SP",
+    content: "Montagem de arte, impressão e instalação de adesivos."
   },
   {
     id: 6,
     image: specialle,
-    title: "Specialle Pizzaria",
-    local: "Bady Bassit - SP",
-    content: "Montagem de Arte, Impressão e Instalação de Lona em Estrutura"
+    title: "Pizzaria Specialle",
+    local: "Bady Bassitt - SP",
+    content: "Montagem de arte, impressão e instalação de fachada."
   },
   {
     id: 7,
     image: vetspets,
-    title: "Vet's Pets",
-    local: "Bady Bassit - SP",
-    content: "Montagem de Arte, Impressão e Instalação de Lona em Estrutura"
+    title: "Clínica Vets e Pets",
+    local: "Bady Bassitt - SP",
+    content: "Montagem de arte, impressão e instalação de lona em outdoor."
+  },
+  {
+    id: 8,
+    image: bancabady,
+    title: "Banca de Bady",
+    local: "Bady Bassitt - SP",
+    content: "Montagem de arte, impressão e instalação de fachada."
+  },
+  {
+    id: 9,
+    image: erika,
+    title: "Espaço Erika Ruiz",
+    local: "Bady Bassitt - SP",
+    content: "Montagem de arte, impressão e instalação de fachada."
+  },
+  {
+    id: 10,
+    image: insenf,
+    title: "Clínica Vets e Pets",
+    local: "Bady Bassitt - SP",
+    content: "Montagem de arte, impressão e instalação de lona em outdoor."
+  },
+  {
+    id: 11,
+    image: kadock,
+    title: "Kadock",
+    local: "São José do Rio Preto-SP",
+    content: "Montagem de arte, impressão e instalação de fachada."
+  },
+  {
+    id: 12,
+    image: limalar,
+    title: "Casa Lima Lar",
+    local: "Bady Bassitt - SP",
+    content: "Montagem de arte, impressão e instalação de fachada."
+  },
+  {
+    id: 13,
+    image: venite,
+    title: "Venite Moda Íntima",
+    local: "Bady Bassitt - SP",
+    content: "Montagem de arte, impressão e instalação de fachada."
+  },
+  {
+    id: 14,
+    image: vetspets2,
+    title: "Clínica Vets e Pets",
+    local: "Bady Bassitt - SP",
+    content: "Montagem de arte, impressão e instalação de luminoso."
   },
 ]
