c62e428378790d3f24efc8024dff61050036a8ac
vitordcode <vitordcode@gmail.com>
2023-07-11 12:35:13 -0300

    adicioando alt nos elementos de fundo e alterado z-index linhas atras do botao quem somos

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -12,18 +12,18 @@ export default function Header() {
   return (
     <>
       <a target="_blank" className="fixed whatsapp bottom-4 right-4 w-10" href="https://api.whatsapp.com/send?phone=5517996820989&text=Ol%C3%A1,%20vim%20atrav%C3%A9s%20do%20site!!" >
-        <img src={whatsapp} alt="" />
+        <img src={whatsapp} alt="Botao para Whatsapp" />
       </a>
-      <img src={iconRight} className="absolute right-0 top-[60vh] lg:top-[40vh] lg:w-56" alt="" />
-      <img src={iconTop} className="absolute left-0 top-[10vh] lg:top-[5vh] lg:w-48" alt="" />
-      <img src={bolinhasLeft} className="absolute left-0 top-[40vh] lg:top-[50vh] lg:w-48" alt="" />
+      <img src={iconRight} className="absolute right-0 top-[60vh] lg:top-[40vh] lg:w-56 -z-10" alt="SVG de background" />
+      <img src={iconTop} className="absolute left-0 top-[10vh] lg:top-[5vh] lg:w-48 -z-10" alt="SVG de background" />
+      <img src={bolinhasLeft} className="absolute left-0 top-[40vh] lg:top-[50vh] lg:w-48 -z-10" alt="SVG de background" />
       
       <section id="header" className="max-w-7xl flex flex-col justify-center md:items-center lg:justify-between  lg:items-center mx-auto lg:flex-row-reverse relative z-0 px-10 h-screen">
       <div className=" w-fullf lg:flex-1 h-auto">
         <img className="w-[80%] md:w-[85%] lg:w-[70%] mx-auto lg:ml-auto" src={lampada} alt="Logo EmpreART" />
       </div>
       <div className="max-w-xl relative">
-        <img src={lines} className="absolute left-28 top-[-42px] w-28 lg:top-[-12rem] lg:w-48" alt="" />
+        <img src={lines} className="absolute left-28 top-[-42px] w-28 lg:top-[-12rem] lg:w-48 -z-10" alt="Linhas de fundo" />
         <h1 className="text-white lg:inline md:text-center uppercase font-black text-2xl md:text-4xl lg:text-5xl lg:leading-[4rem] my-12">Você tem a <span>ideia</span> <br />Nós temos a <span>solução</span></h1>
         <div className="md:flex md:items-center md:justify-center lg:inline-block lg:mt-10">
           <Link activeClass="active"
@@ -44,7 +44,7 @@ export default function Header() {
             className="text-white hover:bg-white hover:text-gray-900 transition-all cursor-pointer bg-transparent border-2 px-6 py-[12px] lg:px-12 lg:py-[18px] ml-2 md:ml-6 lg:text-lg rounded-md"
             >Quem somos</Link>
         </div>
-        <img src={lines} className="absolute right-28 lg:left-48 lg:bottom-[-10rem] w-32 lg:w-48" alt="" />
+        <img src={lines} className="absolute right-28 lg:left-48 lg:bottom-[-10rem] w-32 lg:w-48 -z-10" alt="SVG de background" />
       </div>
     </section>
     </>
